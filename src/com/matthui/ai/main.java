461491c2783910a81c8d7d3f42e3a1b28730383a
matthui <matthewyh12@gmail.com>
2019-03-01 09:24:23 -0800

    Update main.java

--- src/com/matthui/ai/main.java
+++ src/com/matthui/ai/main.java
@@ -1,36 +1,92 @@
 package com.matthui.ai;
+
 import java.util.List;
-import java.util.Random;
 
 import com.briansea.gamecabinet.game.Game;
 import com.briansea.gamecabinet.game.Move;
 import com.briansea.gamecabinet.game.Player;
 
-public class main extends Player {
+public class main extends Player{
 
-	private MMNode node;
-	public main() {
-		
-		node = new MMNode();
-		// set my name
-		this.nameProperty().set("Random AI");
-	}
+	private String myTurn;
 	
-	public void makeMove( Game gamestate, List<Move> move) {
-		List<List<Move>> validMoves = gamestate.getValidMoves();
-		
-		node.alphaBeta(5, 10);
-		Random r = new Random();
-		int pick = r.nextInt(validMoves.size());
-		for( Move m : validMoves.get(pick)) {
-			move.add(m);
+	public main () {
+		this.nameProperty().set("checkersAI");
+	}
+	public void makeMove( Game gs, List<Move> move ) {
+		myTurn = gs.whoseTurn();
+		// this function makes the root
+		int alpha = Integer.MIN_VALUE;
+		int beta = Integer.MAX_VALUE;
+		int maxDepth = 8;
+		List<List<Move>> validMoves = gs.getValidMoves();
+		for( List<Move> vm : validMoves ) {
+			Game newG = gs.deepCopy();
+			newG.makeMove(vm);
+			MMNode c = new MMNode(newG, Integer.MIN_VALUE, Integer.MAX_VALUE, false);
+			// Make an MMNode appropriately and run alphabeta on it
+			int rtn = c.alphabeta(c.depth);
+					
+			if( rtn > alpha ) {
+				alpha = rtn;
+				move.clear();
+				// Copy the validMove into move
+			}
 		}
 		
 	}
 	
 	private class MMNode {
-		public int alphaBeta(int alpha, int beta) {
-			return 5;
+		private Game gs;
+		private int alpha;
+		private int beta;
+		private boolean isMax;
+		
+		public MMNode( Game gs, int alpha, int beta, boolean isMax ) {
+			this.gs = gs;
+			this.alpha = alpha;
+			this.beta = beta;
+			this.isMax = isMax;
+		}
+
+		public int alphabeta(int depth ) {
+			if( depth == 0 ) {
+				return this.score();
+			}
+			List<List<Move>> validMoves = this.gs.getValidMoves();
+			for( List<Move> vm : validMoves ) {
+				if( this.alpha >= this.beta ) {
+					//pruning
+					break;
+				}
+				Game newG = gs.deepCopy();
+				newG.makeMove(vm);
+				MMNode c = new MMNode(gs, Integer.MIN_VALUE, Integer.MAX_VALUE, false);
+				int result = c.alphabeta(depth-1);
+				if(c.isMax) {
+					if(result > c.alpha) {
+						c.alpha = result;
+					}
+				} else {
+					if (result < c.beta) {
+						c.beta = result;
+					}
+				}
+				if(c.alpha >= c.beta || (System.currentTimeMillis() - startTime) > 3990) {
+					c.isMax = true;
+				}
+				// Create a child MMNode appropriately and call alphabeta on it		
+				// Use the return to change alpha or beta correctly
+			}
+			if (isMax) {
+				return this.alpha;
+			} else {
+				return this.beta;
+			}
+		}
+		
+		public int score() {
+			return this.gs.getScore(this.gs.getPlayer(myTurn));
 		}
 	}
 }
